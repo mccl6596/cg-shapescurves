ed6776ee1a2d6214f80453adabef76665cb3d7e0
mackenziemcclellan <mccl6596@stthomas.edu>
2022-02-28 13:26:28 -0600

    updates

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -74,7 +74,11 @@ class Renderer {
 
     // ctx:          canvas context
     drawSlide2(ctx) {
-
+        let a = {x: 100, y:100};
+        let a1 = {x: 100, y:300};
+        let b = {x: 300, y:100};
+        let b1 = {x: 300, y:300};
+        this.drawBezierCurve(a, a1, b1, b, [255, 255, 0], ctx);
     }
 
     // ctx:          canvas context
@@ -93,6 +97,12 @@ class Renderer {
         this.drawLine(left_top, right_top, color, ctx);
         this.drawLine(right_bottom, right_top, color, ctx);
         this.drawLine(left_bottom, right_bottom, color, ctx);
+        if (this.show_points == 1) {
+            this.drawPoint(left_top, color, ctx);
+            this.drawPoint(left_bottom, color, ctx);
+            this.drawPoint(right_top, color, ctx);
+            this.drawPoint(right_bottom, color, ctx);
+        }
 
         
     }
@@ -112,9 +122,13 @@ class Renderer {
             newAngle = newAngle + degrees
             if (degrees > 360) {
                 this.drawLine(starting, a, color, ctx);
+                this.drawPoint(starting, color, ctx);
                 break
             }
             placeHolder =  {x: (center.x + (radius * Math.cos(newAngle * Math.PI / 180))),y: (center.y + (radius * Math.sin(newAngle* Math.PI / 180)))};
+            if (this.show_points == 1) {
+                this.drawPoint(placeHolder, color, ctx);
+            }
             this.drawLine(a, placeHolder, color, ctx);
             a = placeHolder;
         }
@@ -129,7 +143,33 @@ class Renderer {
     // color:        array of int [R, G, B, A]
     // ctx:          canvas context
     drawBezierCurve(pt0, pt1, pt2, pt3, color, ctx) {
-        
+        let counter = 1.0/this.num_curve_sections;
+        let t = counter;
+        let starting = {x: pt0.x, y: pt0.y};
+        if (this.show_points == 1) {
+            this.drawPoint(starting, color, ctx);
+        }
+        let placeHolder;
+        for(let i = 0; i < this.num_curve_sections; i++) {
+            placeHolder = {x:(Math.pow((1-t), 3)*pt0.x)+(3*Math.pow((1-t), 2)*t*pt1.x)+(3*(1-t)*Math.pow(t, 2)*pt2.x)+(Math.pow(t, 3)*pt3.x), y: (Math.pow((1-t), 3)*pt0.y)+(3*Math.pow((1-t), 2)*t*pt1.y)+(3*(1-t)*Math.pow(t, 2)*pt2.y)+(Math.pow(t, 3)*pt3.y)};
+            this.drawLine(starting, placeHolder, color, ctx);
+            if (this.show_points == 1) {
+                this.drawPoint(placeHolder, color, ctx);
+            }
+            t = t + counter;
+            starting = placeHolder;
+        }
+    }
+
+    drawPoint(point, color, ctx) {
+        let a = {x:point.x-2, y: point.y-2};
+        let b = {x: point.x-2, y: point.y+2};
+        let c = {x:point.x+2, y: point.y+2};
+        let d = {x: point.x+2, y: point.y-2};
+        this.drawLine(a, b, color, ctx);
+        this.drawLine(b, c, color, ctx);
+        this.drawLine(c, d, color, ctx);
+        this.drawLine(d, a, color, ctx);
     }
 
     // pt0:          object ({x: __, y: __})
